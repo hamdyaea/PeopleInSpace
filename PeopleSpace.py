a4be155f41df6d9c51f1f587aebbaa9fa0ba9145
hamdyaea <hamdy.aea@protonmail.com>
2019-11-09 12:53:49 +0100

    code beauty

--- PeopleSpace.py
+++ PeopleSpace.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
 # Developer : Hamdy Abou El Anein
-
+#
+# hamdy.aea@protonmail.com
+#
 # This Python 3 software show in real-time how many people are in space.
 # Created with the api of http://open-notify.org/Open-Notify-API/People-In-Space/
 
@@ -15,30 +17,40 @@ name = ""
 spacecraft = ""
 total = ""
 
+
 def parser():
     global name, spacecraft, total
-    with open('rawdata', 'r') as f:
+    with open("rawdata", "r") as f:
         people = json.load(f)
         if people["message"] == "success":
-            key = ((people["number"]) - 1)
+            key = (people["number"]) - 1
             keyload = 0
             while keyload <= key:
                 displaykey = key + 1
-                name = name + (("Name of the astronaut : ")+str(people["people"][keyload]["name"])+str("\n\n")+str("Spacecraft : ")+str(people["people"][keyload]["craft"])+str("\n\n"))
+                name = name + (
+                    ("Name of the astronaut : ")
+                    + str(people["people"][keyload]["name"])
+                    + str("\n\n")
+                    + str("Spacecraft : ")
+                    + str(people["people"][keyload]["craft"])
+                    + str("\n\n")
+                )
                 keyload = keyload + 1
                 if keyload > key:
                     break
             image = "spacecraft.gif"
-            msg = (("This software show in real-time how many people are in space.\n\n")\
-                   +str(("Number of people in space : ")+str(people["number"]))\
-                   +str("\n\n")+(name))
+            msg = (
+                ("This software show in real-time how many people are in space.\n\n")
+                + str(("Number of people in space : ") + str(people["number"]))
+                + str("\n\n")
+                + (name)
+            )
             choices = ["Close"]
             reply = buttonbox(msg, image=image, choices=choices)
             if reply == "Close":
                 sys.exit(0)
             else:
                 sys.exit(0)
-
 
         else:
             image = "spacecraft.gif"
@@ -58,12 +70,8 @@ if os.path.exists(filePath):
 else:
     print("")
 
-url = ("http://api.open-notify.org/astros.json")
-filename = wget.download(url,out="rawdata")
-
-
+url = "http://api.open-notify.org/astros.json"
+filename = wget.download(url, out="rawdata")
 
 
 parser()
-
-
